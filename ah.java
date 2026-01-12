bf05cf1ee4799145f3e8758ad7f55b9e287af13c
N0TEthis <t5405656@yandex.ru>
2026-01-12 10:23:09 +0300

    git push

---
+++ ah.java
@@ -0,0 +1,7 @@
+<script>
+// Когда пользователь нажимает на <div>, откройте всплывающее окно
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+</script>
